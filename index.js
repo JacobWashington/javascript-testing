2317b4d9cd5ad89aec3c556fa820eb4f69b13227
jacobwashington <jacobwashington.dev@gmail.com>
2021-02-05 12:31:18 -0800

    added index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+let jacob = "Jacob";
+let age = 28;
+let dwightLike = true;
+const obj = {
+  class: "SEI 111",
+  company: "GA",
+};
+
+const arr = [1, "hello", { name: "John" }, true];
+
+const add = (num1, num2) => {
+  return num1 + num2;
+};
+
+module.exports = {
+  jacob,
+  age,
+  dwightLike,
+  obj,
+  arr,
+  add,
+};
